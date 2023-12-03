daec72a5f18afc77ca3336d42d040ae81221a751
Molly Draven <molly.crendraven@gmail.com>
2023-12-03 11:52:33 -0500

    Add process control

--- server.ts
+++ server.ts
@@ -1,6 +1,93 @@
-import app from "./src/index.js";
+import { Server } from "node:http";
+import {
+  gameServer,
+  loginServer,
+  profileServer,
+  webServer,
+} from "./src/index.js";
+
+let webServerProcess: Server;
+
+let running = false;
 
 /**
- * Start an express server on port 3000
+ * Start the servers
  */
-app.listen(3000, () => console.log("Listening on port 3000"));
+function startServers() {
+  /**
+   * Start an express server on ports 80 and 3000
+   */
+  webServerProcess = webServer.listen(3000, () =>
+    console.log("Listening on port 3000")
+  );
+
+  /**
+   * Start a TCP server that listens on port 8226
+   */
+  loginServer.listen(8226, () => console.log("Listening on port 8226"));
+  profileServer.listen(8227, () => console.log("Listening on port 8227"));
+  gameServer.listen(8228, () => console.log("Listening on port 8228"));
+
+  console.log("Servers started");
+}
+
+/**
+ * Stop the servers
+ */
+function stopServers() {
+  console.log("Stopping servers");
+  console.log("Stopping web server");
+  webServerProcess.close();
+  console.log("Stopping login server");
+  loginServer.close();
+  console.log("Stopping profile server");
+  profileServer.close();
+  console.log("Stopping game server");
+  gameServer.close();
+
+  console.log("Servers stopped");
+  process.stdin.setRawMode(false);
+  process.stdin.pause();
+  process.stdin.removeAllListeners();
+}
+
+/**
+ * Handle keypresses
+ */
+function handleKeypress(keypress: Buffer) {
+  // If x is pressed, stop the servers
+  if (keypress[0] === 120) {
+    running = false;
+    stopServers();
+  }
+}
+
+/**
+ * Main program
+ */
+startServers();
+running = true;
+
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+  process.stdin.on("data", (keypress) => {
+    // If this is a CTRL+C keypress, send SIGINT to the process
+    if (keypress[0] === 3) {
+      process.kill(process.pid, "SIGINT");
+    }
+
+    // Handle the keypress
+    handleKeypress(keypress);
+  });
+}
+
+/**
+ * The main loop
+ */
+while (running) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  console.log("Main loop");
+}
+
+console.log("Program ended");
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes.js";
+import net from "node:net";
 
 /**
  * Start an express server on ports 80 and 3000
  */
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router);
+const webServer = express();
+webServer.use(cors());
+webServer.use(express.json());
+webServer.use(router);
 
 /**
- * Export the express app
+ * Start a TCP server that listens on port 8226
  */
-export default app;
+const loginServer = net.createServer();
+const profileServer = net.createServer();
+const gameServer = net.createServer();
+
+/**
+ * All 3 servers have the same connection handler
+ */
+const connectionHandler = (socket: net.Socket) => {
+  socket.on("data", (data) => {
+    console.log(data);
+  });
+}
+
+loginServer.on("connection", connectionHandler);
+profileServer.on("connection", connectionHandler);
+gameServer.on("connection", connectionHandler);
+
+
+
+
+/**
+ * Export all servers
+ */
+export { webServer, loginServer, profileServer, gameServer };
